d9ba011e1397de5dc2f57b1aaf7eadf37c7db13e
shuvro <shuvro.dhrubo@gmail.com>
2020-07-17 03:36:02 +0600

    modified details

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,22 +6,22 @@
 
 module.exports = {
   siteMetadata: {
-    title: `Baran Cezayirli | Technologist`,
-    author: `Baran Cezayirli`,
+    title: `Nazmul (Shuvro) Hoque Khan | Software Engineer`,
+    author: `Nazmul Hoque Khan`,
     description: `With 10+ years of experience in various industries, I help tech
               startups from idea phase to high volumes. I design and build
               scalable solutions, implement internal processes that fit your
               organization, build teams that produce results.`,
-    siteUrl: `https://barancezayirli.com/`,
+    siteUrl: `https://shuvro.me/`,
     social: {
-      twitter: `barancezayirli`,
+      twitter: `cse_shuvro`,
     },
   },
   plugins: [
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Baran Cezayirli | Technologist`,
+        name: `Nazmul (Shuvro) Hoque Khan | Software Engineer`,
         short_name: `CV`,
         start_url: `/`,
         background_color: `#f7f0eb`,
